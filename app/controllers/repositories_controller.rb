1b9db4552ad8a13346271e3127a336e5a7b4e29a
geoffrey woodcock <theatomproject@gmail.com>
2019-06-10 19:10:25 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,7 +6,7 @@ class RepositoriesController < ApplicationController
 
   def github_search
      @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
-        req.params['client_id'] = 476eefe204e368e2039e
+        req.params['client_id'] = "476eefe204e368e2039e"
         req.params['client_secret'] = b79f80136c1e51fbd1d5c08bc553a6171212381c
         req.params['q'] = params[:query]
     end
